92d2de31b8c235561628acf799b599d7114fe057
jmbinger <juliabinger94@gmail.com>
2015-12-06 17:39:18 -0500

    change

--- src/com/datalgo/AlgTest.java
+++ src/com/datalgo/AlgTest.java
@@ -8,7 +8,7 @@ public class AlgTest {
 	@Test
 	public void test() {
 		Main main = new Main();
-		Line[] lines = main.genLines(200000);
+		Line[] lines = main.genLines(20000);
 		main.bruteForce(lines);
 	}
 
