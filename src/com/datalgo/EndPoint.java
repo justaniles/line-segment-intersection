c1be90c9d3c46ab73f8722715df252eda8d9878c
jmbinger <juliabinger94@gmail.com>
2015-12-06 16:31:56 -0500

    EndPoint and Line

---
+++ src/com/datalgo/EndPoint.java
@@ -0,0 +1,18 @@
+public class EndPoint {
+	private int xCoord;
+	private int yCoord;
+	
+	public EndPoint(int x, int y) {
+		xCoord = x;
+		yCoord = y;
+	}
+	
+	public int x() {
+		return xCoord;
+	}
+	
+	public int y() {
+		return yCoord;
+	}
+	
+}
---
+++ src/com/datalgo/Line.java
@@ -0,0 +1,25 @@
+public class Line {
+	private EndPoint start;
+	private EndPoint end;
+	private Boolean isHorizontal;
+	
+	//assume first and second are in order of increasing x or decreasing y
+	public Line(EndPoint first, EndPoint second, Boolean isHoriz) {
+		start = first;
+		end = second;
+		isHorizontal = isHoriz;
+	}
+	
+	public EndPoint start() {
+		return start;
+	}
+	
+	public EndPoint end() {
+		return end;
+	}
+	
+	public Boolean isHoriz() {
+		return isHorizontal;
+	}
+	
+}
