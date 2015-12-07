a284a7f488e56c1d3cbe5d839dbaf568808d88bf
jmbinger <juliabinger94@gmail.com>
2015-12-06 19:22:20 -0500

    main

--- src/com/datalgo/Main.java
+++ src/com/datalgo/Main.java
@@ -1,8 +1,20 @@
 package com.datalgo;
 
+import java.io.PrintWriter;
+
 public class Main {
 
     public static void main(String[] args) {
+        //generate graph of speeds for  n = {10, 100, 500, 1000, 2000, 10000, 20000}
+        int[] nValues = {10, 100, 500, 1000, 2000, 10000, 20000};
+        PrintWriter bf = new PrintWriter("brute_force.txt", "UTF-8");
+        
+
+        for(int n : nValues) {
+            Line[] lines = GenerateLines.genLines(n);
+            BruteForce.bruteForce(lines);
+
+        }
 
     }
 
