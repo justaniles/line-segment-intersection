0e8b862c514cedd8365135b36cbdea378fe051c3
Justin Niles <justin.niles12@gmail.com>
2015-12-08 13:37:36 -0500

    Update Algorithm

--- src/com/datalgo/Algorithm.java
+++ src/com/datalgo/Algorithm.java
@@ -3,47 +3,61 @@ package com.datalgo;
 import java.util.Arrays;
 import java.util.Comparator;
 
-/**
- * Created by justinniles on 12/6/15.
- */
 public class Algorithm {
 
     public static void efficientAlgorithm(EndPoint[] endPoints) {
-        // Sort endPoints in descending y-coord
-        Arrays.sort(endPoints, new YCoordComparator());
+        // Sort endPoints in ascending y-coord
+        Arrays.sort(endPoints, new SortVertical());
 
-        BPTree<Integer, EndPoint> bptree =
-                new BPTree<Integer, EndPoint>(new XCoordComparator(), 3, 1);
+        AVL<Integer, EndPoint> statusLine = new AVL<>();
+        Line parentLine;
+        for (EndPoint currentEndPt : endPoints) {
+            parentLine = currentEndPt.getParentLine();
+
+            // Horizontal line:
+            // Perform range search and print out intersecting vertical lines.
+            if (parentLine.isHoriz()) {
+                
+            }
+            // Vertical line:
+            // If this is the starting (top) endpt, add to statusLine,
+            // otherwise delete.
+            else {
+                if (currentEndPt.equals(parentLine.start())) {
+                    statusLine.insert(currentEndPt.x(), currentEndPt);
+                }
+                else {
+                    statusLine.delete(currentEndPt.x(), currentEndPt);
+                }
+            }
+        }
+
+
 
     }
 
 }
 
-class YCoordComparator implements Comparator<EndPoint> {
+class SortVertical implements Comparator<EndPoint> {
+
+    /**
+     * This function will be used to sort the endPoint array with the largest
+     * y-coordinate first. Doing so will enable us to traverse the endPoints
+     * in a top-to-bottom manner.
+     * @param a
+     * @param b
+     * @return
+     */
     @Override
     public int compare(EndPoint a, EndPoint b) {
         if (a.y() < b.y()) {
-            return -1;
+            return 1;
         }
         else if (a.y() == b.y()) {
             return 0;
         }
         else {
-            return 1;
-        }
-    }
-}
-class XCoordComparator implements Comparator<Integer> {
-    @Override
-    public int compare(Integer a, Integer b) {
-        if (a < b) {
             return -1;
-        }
-        else if (a == b) {
-            return 0;
-        }
-        else {
-            return 1;
         }
     }
 }
